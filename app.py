15cefeaa1aa2230f457a5172a8f4a228c168460f
xyz <160399249+vgrfl@users.noreply.github.com>
2022-02-20 09:49:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+
+@st.cache_data
+def read_SP500():
+    return pd.read_csv('SnP500.csv')
+
+try:
+    sp500_df = read_SP500()
+except:
+    st.exception(RuntimeError("Failed to Load the Required local Dataset"))
+    exit()
+
+
+pg1_title = "Symbol list of S&P500 stocks"
+
+with st.sidebar:
+    #st.subheader("Page Selection")
+    page = st.radio("Select a Section to view", ([pg1_title]))
+
+
+if page == pg1_title:
+    st.subheader("S&P 500 Stock Symbols & Company Names")
+    st.dataframe(sp500_df.drop(columns="Sector"), height=600, width=500)
